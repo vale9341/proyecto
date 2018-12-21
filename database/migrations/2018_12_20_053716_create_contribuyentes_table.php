180e4a0de92f89eb1ccb960738d9e4006ae74009
vale9341 <46042577+vale9341@users.noreply.github.com>
2018-12-21 02:01:05 -0600

    Add files via upload

---
+++ database/migrations/2018_12_20_053716_create_contribuyentes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContribuyentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contribuyentes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('razonSocial');
+            $table->string('RFC');
+            $table->string('correo_Electronico');
+            $table->integer('id_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contribuyentes');
+    }
+}
